6d4c6e4144697da6f1e8300e68c5c328c2a8433e
regiindrawan <regiindrawan110@gmail.com>
2026-07-20 15:31:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const hiddenElements = document.querySelectorAll("section");
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el)=>{
+
+    el.classList.add("hidden");
+
+    observer.observe(el);
+
+});
