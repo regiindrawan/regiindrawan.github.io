75701b5a6a8d2cdf7d3e2d9aef26d50f0c3427e1
regiindrawan <regiindrawan110@gmail.com>
2026-07-23 12:48:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,32 @@ const skillObserver = new IntersectionObserver((entries) => {
 skillBars.forEach(bar => {
     skillObserver.observe(bar);
 });
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
