695fd5b305cfae8c239209a20c24928948011b73
regiindrawan <regiindrawan110@gmail.com>
2026-07-30 12:32:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -147,3 +147,26 @@ function changeProductImage(image){
     image.classList.add("active");
 
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if(target){
+
+            target.scrollIntoView({
+
+                behavior:"smooth",
+
+                block:"start"
+
+            });
+
+        }
+
+    });
+
+});
